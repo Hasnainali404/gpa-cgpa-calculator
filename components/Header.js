94c23248072d46a2b18cdafcee8745f3f65e22c8
Hasnainali404 <jeinn467@gmail.com>
2026-02-15 15:14:48 +0500

    ready the project

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+
+export default function Header() {
+
+    return (
+        <>
+           <header className="flex flex-row items-center justify-between ">
+             <div>
+                <div>left</div>
+             <div>right</div>
+             </div>
+           </header>
+        </>
+    )
+}
+
+
+
+
+// export default function Home() {
+//   return (
+//    <>
+//     <div>
+//       Hello Dev
+//     </div>
+//    </>
+//   );
+// }
